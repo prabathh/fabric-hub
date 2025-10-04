ad83c658d05308d4050dd31db77bb2d3bd5a02f1
Niroshan Selvaraj <122067370+niro17@users.noreply.github.com>
2025-10-04 21:02:12 +0530

    Update button (Dark Theme)

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -14,7 +14,8 @@ export interface ButtonProps
     | "secondary"
     | "success"
     | "attention"
-    | "disabled";
+    | "disabled"
+    | "dark"; 
   size?: "small" | "medium" | "large";
 }
 
@@ -32,17 +33,22 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ) {
     const mergedClassName = twMerge(
       clsx(
-        "flex items-center justify-center gap-1 rounded-lg font-bold uppercase",
+        "flex items-center justify-center gap-1 rounded-lg font-bold uppercase transition-colors duration-200 focus:outline-none focus:ring-2",
         {
           ["text-sm px-3 py-2"]: size === "small",
           ["px-4 py-3 text-sm"]: size === "medium",
           ["px-6 py-4 text-base"]: size === "large",
+
           [`${COLORS.primary.bg} ${COLORS.primary.text} ${COLORS.primary.hoverBg} ${COLORS.primary.ring}`]:
             !disabled && theme === "primary",
           [`${COLORS.secondary.bg} ${COLORS.secondary.text} ${COLORS.secondary.hoverBg} ${COLORS.secondary.ring}`]:
             !disabled && theme === "secondary",
           [`${COLORS.success.bg} ${COLORS.success.text} ${COLORS.success.hoverBg} ${COLORS.success.ring}`]:
             !disabled && theme === "success",
+
+          // Dark theme
+          ["bg-black text-white hover:bg-neutral-800 focus:ring-neutral-600"]:
+            !disabled && theme === "dark",
         },
         className
       )
